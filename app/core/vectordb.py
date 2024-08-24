673bb8f11b85fa637368c7e205c1a28a0ac6d509
中野三玖 <heyuehuii@126.com>
2024-08-24 16:08:13 +0800

    (chore) clean vectordb

--- app/core/vectordb.py
+++ app/core/vectordb.py
@@ -14,15 +14,9 @@ def get_vectordb():
     else:
         embedding = HuggingFaceEmbeddings(model_name=setting.EMBEDDING_PATH)
     vector_store = Chroma.from_texts(
-        ['张强强毕业于兰州大学，今年28岁，喜欢唱，跳，rap，篮球',
-         '贺月辉毕业于北京邮电大学，今年25岁，喜欢羽毛球，电影，写代码',
-         '代镜毕业于重庆大学，今年27岁，喜欢做饭，唱歌，睡觉',
-         '张强强和贺月辉以及代镜目前在同一家单位就职，是非常好的朋友'],
+        [''],
         metadatas=[
             {'author': '张强强'},
-            {'author': '贺月辉'},
-            {'author': '代镜'},
-            {'author': '贺月辉'}
         ],
         embedding=embedding
     )
